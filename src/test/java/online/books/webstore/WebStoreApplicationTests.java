0ce80f4ea49560d4d13b04e2cd0d334dedd7a4d0
Melnychuk <melnychukmyhailo@gmail.com>
2023-10-17 16:52:25 +0300

    Project created.

---
+++ src/test/java/online/books/webstore/WebStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package online.books.webstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
