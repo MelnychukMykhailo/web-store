3a3fb980ccec4748f1b2619d35985f0be7f8d5a4
Melnychuk <melnychukmyhailo@gmail.com>
2023-11-20 11:23:17 +0200

    User model created

---
+++ src/main/java/online/books/webstore/model/User.java
@@ -0,0 +1,36 @@
+package online.books.webstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Data
+@RequiredArgsConstructor
+@Entity
+@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false, name = "first_name")
+    private String firstName;
+    @Column(nullable = false, name = "last_name")
+    private String lastName;
+    @Column(name = "shipping_address")
+    private String shippingAddress;
+    @Column(nullable = false, name = "is_deleted")
+    private boolean isDeleted = false;
+}
